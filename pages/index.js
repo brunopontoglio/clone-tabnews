dbc8af18e00a22b7f5a5b57548b172f2b5961bff
Bruno Pontoglio <rodrigues.r.bruno@gmail.com>
2026-04-28 19:29:52 -0300

    Modificando frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>página inicial ajustada corretamente dessa vez</h1>
+    return <h1>Marlene, meu amor, eu te amo! Se você me ama, sorria para mim!</h1>
 }
 
 export default Home;
